6bfe83a6fa31fef03f5d366ba0f3af2836185b72
anagrahari <anagrahari@cs.stonybrook.edu>
2017-04-29 17:37:10 -0400

    implementation

---
+++ src/main/java/com/hadoop/parallel/Hadoop_PageRank/Phase1Mapper.java
@@ -0,0 +1,22 @@
+package com.hadoop.parallel.Hadoop_PageRank;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+
+import java.io.IOException;
+
+public class Phase1Mapper extends Mapper<LongWritable, Text, Text, Text> {
+    
+    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+
+    	if (value.charAt(0) != '#') {
+        	String[] nodes = value.toString().split(" ");
+            String nodeFrom = nodes[0];
+            String nodeTo = nodes[1];
+            context.write(new Text(nodeFrom), new Text(nodeTo));
+        }
+ 
+    }
+    
+}
---
+++ src/main/java/com/hadoop/parallel/Hadoop_PageRank/Phase1Reducer.java
@@ -0,0 +1,28 @@
+package com.hadoop.parallel.Hadoop_PageRank;
+
+import java.io.IOException;
+
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Reducer;
+
+public class Phase1Reducer extends Reducer<Text, Text, Text, Text> {
+    
+    public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+        
+        /* 
+         * Output <PageA>    <page-rank of PageA>    PageX,PageY,PageZ,...
+         */
+        
+        boolean first = true;
+        String links = 1 + "\t";
+        for (Text value : values) {
+            if (!first) 
+                links += ",";
+            links += value.toString();
+            first = false;
+        }
+
+        context.write(key, new Text(links));
+    }
+
+}
